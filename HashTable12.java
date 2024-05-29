c564867b836a9b52c4e0a65410357de8c9fbfab1
KaijuKoders <avishkar.yelave@nciportal.com>
2024-05-29 09:28:17 +0530

    first commit

---
+++ HashTable12.java
@@ -0,0 +1,20 @@
+package com.map;
+
+import java.util.Comparator;
+import java.util.Hashtable;
+import java.util.TreeMap;
+
+public class HashTable 
+{
+	public static void main(String[] args) 
+	{
+		Hashtable hash = new Hashtable();
+		
+		hash.put(106,"Avishkar");
+		hash.put(117, "Nana");
+		hash.put(103, "Onkar");
+		
+		System.out.println(hash);
+			
+	}
+}
